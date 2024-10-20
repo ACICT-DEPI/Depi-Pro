f5013d6b56cc07a7ad45a58797c90af2502c715e
ACICT <alber.aziz@acict.com>
2024-10-20 15:29:52 +0300

    3:30

---
+++ project/Software Development/.Net Web Developer/SHR1_SWD5_S1e/GameZone-main/GameZone/GameZone/Controllers/RolesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameZone.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        //Create a Role
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> New(RoleViewModel newRole)
+        {
+            if (ModelState.IsValid) 
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = newRole.RoleName;
+
+              IdentityResult result= await roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    TempData["SuccessMessage"] = "Role created successfully!";
+                    return View(new RoleViewModel());
+                }
+                foreach (var item in result.Errors)
+                { 
+                    ModelState.AddModelError("",item.Description);
+                }
+            }
+            return View(newRole);
+
+
+        }
+
+       
+
+    }
+}
